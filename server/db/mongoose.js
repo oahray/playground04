a31e03583b1e3cc8217ce9617aefd908a0054881
Oahray <rodneyoare@gmail.com>
2017-06-09 13:16:25 +0100

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,18 @@
 var mongoose = require('mongoose');
 
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+
+// var localDB = 'mongodb://127.0.0.1:27017/TodoApp';
+// var remoteDB = 'mongodb://todo-api:todo-api@ds115712.mlab.com:15712/node-todo-api'
+// process.env.MONGODB_URI 
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp', options);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Mongo connection error: '));
 
 module.exports = {mongoose};
+
+//  ?authSource=admin
