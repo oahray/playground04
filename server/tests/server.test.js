5fdccad066047eaede2038db50c51da51e5cd481
Oahray <rodneyoare@gmail.com>
2017-06-10 13:09:52 +0100

    Add test for PATCH /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
   text: "First test todo"
 }, {
   _id: new ObjectID(),
-  text: "Second test todo"
+  text: "Second test todo",
+  completed: true,
+  completedAt: 333
 }];
 
 beforeEach((done) => {
@@ -132,6 +134,59 @@ describe('DELETE /todos/:id', () => {
   it('should return 404 for object id not valid', (done) => {
     request(app)
       .delete('/todos/123')
+      .expect(404)
+      .end(done);
+  });
+});
+
+describe('PATCH /todos/:id', () => {
+  it('should update the todo', (done) => {
+    var hexId = todos[0]._id.toHexString();
+    var text = "First test todo patched";
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        text,
+        completed: true
+      })
+      .expect(200)
+      .expect((todo) => {
+        expect(todo.body.todo.text).toBe(text);
+        expect(todo.body.todo.completed).toBe(true);
+        expect(todo.body.todo.completedAt).toNotBe(null).toBeA('number');
+      })
+      .end(done);
+  });
+
+  it('should update clear completedAt when todo is not completed', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    var text = "Second test todo patched";
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        text, 
+        completed: false
+      })
+      .expect(200)
+      .expect((todo) => {
+        expect(todo.body.todo.text).toBe(text);
+        expect(todo.body.todo.completed).toBe(false);
+        expect(todo.body.todo.completedAt).toBe(null);
+      })
+      .end(done);
+  });
+
+  it('should return 404 for todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for object id not valid', (done) => {
+    request(app)
+      .patch('/todos/123')
       .expect(404)
       .end(done);
   });
