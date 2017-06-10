e824872c98592d060f7f4769a854993dec9e568c
Oahray <rodneyoare@gmail.com>
2017-06-10 13:46:00 +0100

    Setup separate development and Test env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Mongo connection error: '));
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const config = require('./config/config');
+
 const express = require('express');
 const bodyParser = require('body-parser')
 const {ObjectID} = require('mongodb');
@@ -8,7 +10,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT
 
 app.use(bodyParser.json());
 
@@ -28,10 +30,8 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find({}).then((todos) => {
-    console.log(todos);
     res.send({todos});
   }, (e) => {
-    console.log('Error getting Todos: ', e)
     res.status(400).send(e);
   });
 });
