e58426834195b9c864a77ff45895581f70b3c3b9
Oahray <rodneyoare@gmail.com>
2017-06-12 18:06:33 +0100

    add POST /users route and generate-token method

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ var port = process.env.PORT
 
 app.use(bodyParser.json());
 
+//TODOS
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -86,6 +87,20 @@ app.patch('/todos/:id', (req, res) => {
     }
     res.send({todo});
   }).catch((e) => res.status(400).send());
+});
+
+//USERS
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.listen(port, () => {
