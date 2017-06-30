52e0087778f7f6bd942379a7b8001ca5cf11b5b2
Oahray <rodneyoare@gmail.com>
2017-06-30 12:43:58 +0100

    Move config to separate JSON file

--- server/config/config.js
+++ server/config/config.js
@@ -9,11 +9,3 @@ if (env === 'development' || env === 'test') {
     process.env[key] = envConfig[key];
   });
 }
-
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
-}
